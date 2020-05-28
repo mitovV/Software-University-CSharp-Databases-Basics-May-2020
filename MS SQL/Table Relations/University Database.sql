b7ab3c97e8b16499762940bcf468f1e2555d6c55
CodingMania <vasilmitovv@gmail.com>
2020-05-28 16:19:14 +0300

    Initial commit

---
+++ MS SQL/Table Relations/University Database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Majors(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50),
+)
+
+CREATE TABLE Students(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber VARCHAR(30),
+	StudentName VARCHAR(30),
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments(
+	PaymentID INT PRIMARY KEY IDENTITY,
+	PaymentDate SMALLDATETIME,
+	PaymentAmount DECIMAL(10,2),
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(30)
+)
+
+CREATE TABLE Agenda(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+	PRIMARY KEY(StudentID, SubjectID)
+)
