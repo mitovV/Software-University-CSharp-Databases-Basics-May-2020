b14cf0b69bd0b4c85beb30392ea60c5bb4ecbaa0
CodingMania <vasilmitovv@gmail.com>
2020-05-28 14:58:18 +0300

    Initial commit

---
+++ MS SQL/Table Relations/One-To-One Relationship.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Passports(
+	PassportID INT PRIMARY KEY IDENTITY,
+	PassportNumber VARCHAR(50)
+)
+
+
+CREATE TABLE Persons(
+	PersonID INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(50) NOT NULL,
+	Salary DECIMAL(10,2),
+	PassportID INT FOREIGN KEY REFERENCES  Passports(PassportID)
+)
