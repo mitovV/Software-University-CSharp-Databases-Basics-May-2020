cd7dc438bc6dcef8a387633eb29cc1716dfa1668
CodingMania <vasilmitovv@gmail.com>
2020-07-01 10:15:21 +0300

    Solve task 7

--- Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
+++ Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -152,5 +152,31 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var addresses = context
+                .Addresses
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeeCount = a.Employees.Count
+                })
+                .OrderByDescending(a => a.EmployeeCount)
+                .ThenBy(a => a.TownName)
+                .ThenBy(a => a.AddressText)
+                .Take(10)
+                .ToList();
+
+            foreach (var addres in addresses)
+            {
+                sb.AppendLine($"{addres.AddressText}, {addres.TownName} - {addres.EmployeeCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
