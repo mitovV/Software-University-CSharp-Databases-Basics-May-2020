6944093a276c21004f7e8b40371a1daebb85b9ae
CodingMania <vasilmitovv@gmail.com>
2020-07-01 10:25:16 +0300

    Solve task 9

--- Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
+++ Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -178,5 +178,44 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var departments = context
+                .Departments
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .Select(d => new
+                {
+                    d.Name,
+                    ManagerFullName = d.Manager.FirstName + " " + d.Manager.LastName,
+                    Employees = d.Employees.Select(e => new
+                    {
+                        e.FirstName,
+                        e.LastName,
+                        e.JobTitle
+                    })
+                    .OrderBy(e => e.FirstName)
+                    .ThenBy(e => e.LastName)
+                    .ToList()
+                })
+                .ToList();
+
+            foreach (var department in departments)
+            {
+                sb.AppendLine($"{department.Name} - {department.ManagerFullName}");
+
+                foreach (var employee in department.Employees)
+                {
+                    var fullName = employee.FirstName + " " + employee.LastName;
+                    sb.AppendLine($"{fullName} - {employee.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
