6410649a8215c6be10f1d0a33774def07547412b
CodingMania <vasilmitovv@gmail.com>
2020-07-01 09:55:13 +0300

    Solved first and second task

---
+++ Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace SoftUni
+{
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employyes = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => sb.AppendLine($"{string.Join(" ", e.FirstName, e.LastName, e.MiddleName)} {e.JobTitle} {e.Salary:F2}"))
+                .ToArray();
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
