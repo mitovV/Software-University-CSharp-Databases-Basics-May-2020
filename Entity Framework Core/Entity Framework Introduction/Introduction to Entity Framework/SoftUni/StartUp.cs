f63eaed672d4e12a1414d1e6da114fe429bc1347
CodingMania <vasilmitovv@gmail.com>
2020-07-01 10:19:04 +0300

    Solve task 7

--- Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
+++ Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -178,5 +178,30 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employee = context
+                .Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects
+                                .Select(p => p.Project.Name)
+                                .OrderBy(p => p)
+                                .ToList()
+                })
+                .FirstOrDefault();
+
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+            sb.AppendLine(string.Join(Environment.NewLine, employee.Projects));
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
