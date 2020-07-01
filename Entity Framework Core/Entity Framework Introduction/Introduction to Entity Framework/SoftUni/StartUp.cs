0a9ab2595afd9700204f5e34796e459da7cefc9e
CodingMania <vasilmitovv@gmail.com>
2020-07-01 11:45:43 +0300

    Solve task 13

--- Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
+++ Entity Framework Core/Entity Framework Introduction/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -298,5 +298,39 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var project = context
+                .Projects
+                .Find(2);
+
+            var eP = context
+                .EmployeesProjects
+                .Where(p => p.ProjectId == project.ProjectId);
+
+            foreach (var item in eP)
+            {
+                context.EmployeesProjects.Remove(item);
+            }
+
+            context.Projects.Remove(project);
+            context.SaveChanges();
+
+            var projects = context
+                .Projects
+                .Take(10)
+                .Select(e => e.Name)
+                .ToList();
+
+            foreach (var projectName in projects)
+            {
+                sb.AppendLine(projectName);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
