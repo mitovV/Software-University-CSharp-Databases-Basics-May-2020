32b21ea24e6a62dc0e86c49218da4a2633e325e2
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:31:15 +0300

    Solve task 4

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -64,5 +64,17 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var titles = context
+                .Books
+                .OrderBy(b => b.BookId)
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, titles);
+        }
     }
 }
