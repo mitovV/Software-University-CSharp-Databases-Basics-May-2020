908d287e7918bbddc9343bf83443e7b1bc7e8b73
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:42:34 +0300

    Solve task 12

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -200,5 +200,30 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var result = context
+                .Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    Profit = c.CategoryBooks
+                    .Select(cb => cb.Book.Price * cb.Book.Copies)
+                    .Sum()
+                })
+                .OrderByDescending(r => r.Profit)
+                .ThenBy(r => r.Name)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var item in result)
+            {
+                sb.AppendLine($"{item.Name} ${item.Profit:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
