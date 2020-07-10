888d9a392bdd03dbe653d90ac98ed2949834b2ab
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:43:50 +0300

    Solve task 13

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -225,5 +225,42 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            var categories = context
+                .Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    Books = c.CategoryBooks
+                    .OrderByDescending(b => b.Book.ReleaseDate)
+                    .Take(3)
+                    .Select(cb => new
+                    {
+                        cb.Book.Title,
+                        cb.Book.ReleaseDate.Value.Year
+                    })
+
+                    .OrderByDescending(b => b.Year)
+                    .ToArray()
+                })
+                .OrderBy(c => c.Name)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var c in categories)
+            {
+                sb.AppendLine($"--{c.Name}");
+
+                foreach (var b in c.Books)
+                {
+                    sb.AppendLine($"{b.Title} ({b.Year})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
