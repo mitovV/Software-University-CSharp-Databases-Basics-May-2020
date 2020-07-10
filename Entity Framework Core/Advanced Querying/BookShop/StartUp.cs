7cae975dad2f3d8a8c12aa8e6665de8d405ba5f7
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:36:13 +0300

    Solve task 8

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -130,5 +130,19 @@
 
             return String.Join(Environment.NewLine, names);
         }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            input = input.ToLower();
+
+            var titles = context
+                .Books
+                .Where(b => b.Title.ToLower().Contains(input))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, titles);
+        }
     }
 }
