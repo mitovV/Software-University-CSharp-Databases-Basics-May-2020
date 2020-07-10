c900ad3e894b74859ea60f8a4705bc98f32267d6
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:26:13 +0300

    Solve task 1

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -1,5 +1,8 @@
 ﻿namespace BookShop
 {
+    using System;
+    using System.Linq;
+
     using Data;
     using Initializer;
 
@@ -9,6 +12,20 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            command = command.ToLower();
+
+            var titles = context
+                .Books
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, titles);
         }
     }
 }
