40602c0a790bbec34d44b5e4303f799b302f46cf
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:35:02 +0300

    Solve task 7

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -118,5 +118,17 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            var names = context
+                .Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => $"{a.FirstName} {a.LastName}")
+                .OrderBy(a => a)
+                .ToList();
+
+            return String.Join(Environment.NewLine, names);
+        }
     }
 }
