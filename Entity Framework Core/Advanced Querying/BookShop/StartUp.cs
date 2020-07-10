dafce1136d339503a272182cc9aa508d21ecba5f
CodingMania <vasilmitovv@gmail.com>
2020-07-10 09:32:38 +0300

    Solve task 5

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -76,5 +76,21 @@
 
             return String.Join(Environment.NewLine, titles);
         }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            var categories = input
+                .ToLower()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var titles = context
+                .Books
+                .Where(c => c.BookCategories.Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+            .ToArray();
+
+            return String.Join(Environment.NewLine, titles);
+        }
     }
 }
