6ca005a8acac4d8afc773395d0c6e5d351b47828
CodingMania <vasilmitovv@gmail.com>
2020-07-17 15:31:13 +0300

    Solve task 8

--- Entity Framework Core/JavaScript Object Notation - JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -118,5 +118,43 @@
 
             return JsonConvert.SerializeObject(categories, Formatting.Indented);
         }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context
+                .Users
+                .Where(u => u.ProductsSold.Any(p => p.Buyer != null))
+                .OrderByDescending(u => u.ProductsSold.Count(p => p.Buyer != null))
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    age = u.Age,
+                    soldProducts = new
+                    {
+                        count = u.ProductsSold.Count(p => p.Buyer != null),
+                        products = u.ProductsSold
+                        .Where(p => p.Buyer != null)
+                        .Select(p => new
+                        {
+                            name = p.Name,
+                            price = p.Price
+                        })
+                        .ToArray()
+                    }
+                })
+                .ToArray();
+
+            var result = new
+            {
+                usersCount = users.Length,
+                users
+            };
+
+            return JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+        }
     }
 }
