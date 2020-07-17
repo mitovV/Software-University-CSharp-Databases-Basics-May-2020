9d0750221ef3469b6ced0ff65165190c78b7f9a1
CodingMania <vasilmitovv@gmail.com>
2020-07-17 15:39:16 +0300

    Solve task 11

--- Entity Framework Core/JavaScript Object Notation - JSON/08. JSON-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. JSON-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
@@ -35,5 +35,40 @@
 
             return $"Successfully imported {parts.Length}.";
         }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var cars = JsonConvert.DeserializeObject<ImportCarDto[]>(inputJson);
+
+            foreach (var car in cars)
+            {
+                var newCar = new Car()
+                {
+                    Make = car.Make,
+                    Model = car.Model,
+                    TravelledDistance = car.TravelledDistance
+                };
+
+                context.Add(newCar);
+
+                foreach (var partId in car.PartsId)
+                {
+                    var partCar = new PartCar()
+                    {
+                        CarId = newCar.Id,
+                        PartId = partId
+                    };
+
+                    if (!newCar.PartCars.Any(pc => pc.PartId == partId))
+                    {
+                        context.PartCars.Add(partCar);
+                    }
+                }
+            }
+
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Length}.";
+        }
     }
 }
