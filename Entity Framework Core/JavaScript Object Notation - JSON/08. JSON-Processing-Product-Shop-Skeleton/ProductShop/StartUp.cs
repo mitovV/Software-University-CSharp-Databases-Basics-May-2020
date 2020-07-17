a55d7036c540c69c075944e177e8e9b1101f79be
CodingMania <vasilmitovv@gmail.com>
2020-07-17 10:32:48 +0300

    Solve task 1, 2, 3

--- Entity Framework Core/JavaScript Object Notation - JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
+++ Entity Framework Core/JavaScript Object Notation - JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -1,18 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
-using ProductShop.Data;
-using ProductShop.Models;
-
-namespace ProductShop
+﻿namespace ProductShop
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    using Data;
+    using Models;
+
+    using Newtonsoft.Json;
+
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+        }
 
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<Category[]>(inputJson)
+                .Where(p => p.Name != null)
+                .ToArray();
+
+            context.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
         }
     }
 }
