8f3a04e712b00b9180b15d940b4c7b1009164ecb
CodingMania <vasilmitovv@gmail.com>
2020-07-21 11:16:52 +0300

    Solve tasks 2, 3

---
+++ Entity Framework Core/XML Processing/ProductShop/ProductShop/Dtos/Import/ImportCategorieDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductShop.Dtos.Import
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Category")]
+    public class ImportCategorieDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+    }
+}
--- Entity Framework Core/XML Processing/ProductShop/ProductShop/StartUp.cs
+++ Entity Framework Core/XML Processing/ProductShop/ProductShop/StartUp.cs
@@ -14,17 +14,13 @@
         private static IMapper mapper;
 
         public static void Main()
-        {
-            var config = new MapperConfiguration(cfg => 
-            {
-                cfg.AddProfile<ProductShopProfile>();
-            });
-
-            mapper = new Mapper(config);
+        {        
         }
 
         public static string ImportUsers(ProductShopContext context, string inputXml)
         {
+            ConfigureMapper();
+
             var serializer = new XmlSerializer(typeof(ImportUserDto[]), new XmlRootAttribute("Users"));
 
             ImportUserDto[] userDtos;
@@ -40,6 +36,58 @@
             context.SaveChanges();
 
             return $"Successfully imported {users.Length}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            ConfigureMapper();
+
+            var serializer = new XmlSerializer(typeof(ImportProductDto[]), new XmlRootAttribute("Products"));
+
+            ImportProductDto[] productDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                productDtos = (ImportProductDto[])serializer.Deserialize(reader);
+            }
+
+            var products = mapper.Map<Product[]>(productDtos);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            ConfigureMapper();
+
+            var serializer = new XmlSerializer(typeof(ImportCategorieDto[]), new XmlRootAttribute("Categories"));
+
+            ImportCategorieDto[] importCategorieDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                importCategorieDtos = (ImportCategorieDto[])serializer.Deserialize(reader);
+            }
+
+            var categories = mapper.Map<Category[]>(importCategorieDtos);
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
+        }
+
+        private static void ConfigureMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = new Mapper(config);
         }
     }
 }
